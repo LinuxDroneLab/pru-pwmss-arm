7c1599bf274cc5f5760b2e932268ff96ba39d501
Andrea Lambruschini <andrea.lambruschini@gmail.com>
2018-08-15 01:08:56 +0200

    Aggiunto comando set-duty.

--- src/pru-pwmss-arm.cpp
+++ src/pru-pwmss-arm.cpp
@@ -159,6 +159,94 @@ void setData3(pollfd* pollfds,
                (uint8_t) readBuf[3]);
     }
 }
+void setDuty(pollfd* pollfds, uint8_t pwmssId, uint16_t duA, uint16_t duB)
+{
+    int result = 0;
+    readBuf[0] = PRU_PWMSS_LIB_CMD_ID;
+    readBuf[1] = PRU_PWMSS_LIB_CMD_SET_DATA;
+    readBuf[2] = pwmssId;
+    uint16_t* data = (uint16_t*)(readBuf+3);
+    data[0]=duA;
+    data[1]=duB;
+    result = write(pollfds[0].fd, readBuf, 7);
+    if (result > 0)
+    {
+        printf("Setting PWMSS %d ...\n", pwmssId);
+    }
+
+    result = read(pollfds[0].fd, readBuf, 4);
+    if (result > 0)
+    {
+        printf("PWMSSs response=%d:%d:%d:%d\n", (uint8_t) readBuf[0],
+               (uint8_t) readBuf[1], (uint8_t) readBuf[2],
+               (uint8_t) readBuf[3]);
+    }
+}
+
+void setDuty2(pollfd* pollfds,
+              uint8_t pwmssId1, uint16_t duA1, uint16_t duB1,
+              uint8_t pwmssId2, uint16_t duA2, uint16_t duB2)
+{
+    int result = 0;
+    readBuf[0] = PRU_PWMSS_LIB_CMD_ID;
+    readBuf[1] = PRU_PWMSS_LIB_CMD_SET_DATA;
+    readBuf[2] = pwmssId1;
+    readBuf[7] = pwmssId2;
+    uint16_t* data1 = (uint16_t*)(readBuf+3);
+    uint16_t* data2 = (uint16_t*)(readBuf+8);
+    data1[1]=duA1;
+    data1[2]=duB1;
+    data2[1]=duA2;
+    data2[2]=duB2;
+    result = write(pollfds[0].fd, readBuf, 12);
+    if (result > 0)
+    {
+        printf("Setting PWMSS ...\n");
+    }
+
+    result = read(pollfds[0].fd, readBuf, 4);
+    if (result > 0)
+    {
+        printf("PWMSSs response=%d:%d:%d:%d\n", (uint8_t) readBuf[0],
+               (uint8_t) readBuf[1], (uint8_t) readBuf[2],
+               (uint8_t) readBuf[3]);
+    }
+}
+void setDuty3(pollfd* pollfds,
+              uint8_t pwmssId1, uint16_t duA1, uint16_t duB1,
+              uint8_t pwmssId2, uint16_t duA2, uint16_t duB2,
+              uint8_t pwmssId3, uint16_t duA3, uint16_t duB3
+              )
+{
+    int result = 0;
+    readBuf[0] = PRU_PWMSS_LIB_CMD_ID;
+    readBuf[1] = PRU_PWMSS_LIB_CMD_SET_DATA;
+    readBuf[2] = pwmssId1;
+    readBuf[7] = pwmssId2;
+    readBuf[12] = pwmssId3;
+    uint16_t* data1 = (uint16_t*)(readBuf+3);
+    uint16_t* data2 = (uint16_t*)(readBuf+8);
+    uint16_t* data3 = (uint16_t*)(readBuf+13);
+    data1[0]=duA1;
+    data1[1]=duB1;
+    data2[0]=duA2;
+    data2[1]=duB2;
+    data3[0]=duA3;
+    data3[1]=duB3;
+    result = write(pollfds[0].fd, readBuf, 17);
+    if (result > 0)
+    {
+        printf("Setting PWMSS ...\n");
+    }
+
+    result = read(pollfds[0].fd, readBuf, 4);
+    if (result > 0)
+    {
+        printf("PWMSSs response=%d:%d:%d:%d\n", (uint8_t) readBuf[0],
+               (uint8_t) readBuf[1], (uint8_t) readBuf[2],
+               (uint8_t) readBuf[3]);
+    }
+}
 void defaultTests(pollfd* pollfds)
 {
     // start all pwmss devices
@@ -217,6 +305,36 @@ void executeSetData3(pollfd* pollfds, uint8_t pwmssId1, uint16_t period1, uint16
         return;
     }
     setData3(pollfds, pwmssId1, period1, duA1, duB1, pwmssId2, period2, duA2, duB2, pwmssId3, period3, duA3, duB3);
+}
+
+
+void executeSetDuty(pollfd* pollfds, uint8_t pwmssId, uint16_t duA, uint16_t duB)
+{
+    printf("cmpA: %d, cmpB: %d\n", duA, duB);
+    if(pwmssId > 2) {
+        printf("Usage: pru-pwmss-arm set-duty <pwmss id><cmp A><cmp B>");
+        return;
+    }
+    setDuty(pollfds, pwmssId, duA, duB);
+}
+void executeSetDuty2(pollfd* pollfds, uint8_t pwmssId1, uint16_t duA1, uint16_t duB1, uint8_t pwmssId2, uint16_t duA2, uint16_t duB2)
+{
+    if(pwmssId1 > 2 || pwmssId2 > 2 ) {
+        printf("Usage: pru-pwmss-arm set-duty <pwmss id><cmp A><cmp B>");
+        return;
+    }
+    setDuty2(pollfds, pwmssId1, duA1, duB1, pwmssId2, duA2, duB2);
+}
+
+void executeSetDuty3(pollfd* pollfds, uint8_t pwmssId1, uint16_t duA1, uint16_t duB1,
+                                      uint8_t pwmssId2, uint16_t duA2, uint16_t duB2,
+                                      uint8_t pwmssId3, uint16_t duA3, uint16_t duB3)
+{
+    if(pwmssId1 > 2 || pwmssId2 > 2 || pwmssId3 > 2 ) {
+        printf("Usage: pru-pwmss-arm set-duty <pwmss id><cmp A><cmp B>");
+        return;
+    }
+    setDuty3(pollfds, pwmssId1, duA1, duB1, pwmssId2, duA2, duB2, pwmssId3, duA3, duB3);
 }
 
 int main(int argc, char *argv[])
@@ -293,6 +411,42 @@ int main(int argc, char *argv[])
             executeSetData3(pollfds, atoi(devNumStr1), atoi(periodStr1), atoi(duAStr1), atoi(duBStr1),
                                      atoi(devNumStr2), atoi(periodStr2), atoi(duAStr2), atoi(duBStr2),
                                      atoi(devNumStr3), atoi(periodStr3), atoi(duAStr3), atoi(duBStr3));
+        }
+        else if ((strcmp("set-duty", argv[1]) == 0) && (argc == 5))
+        {
+            const char* devNumStr = argv[2];
+            const char* duAStr = argv[3];
+            const char* duBStr = argv[4];
+            executeSetDuty(pollfds, atoi(devNumStr), atoi(duAStr), atoi(duBStr));
+        }
+        else if ((strcmp("set-duty", argv[1]) == 0) && (argc == 8))
+        {
+            const char* devNumStr1 = argv[2];
+            const char* duAStr1 = argv[3];
+            const char* duBStr1 = argv[4];
+
+            const char* devNumStr2 = argv[5];
+            const char* duAStr2 = argv[6];
+            const char* duBStr2 = argv[7];
+            executeSetDuty2(pollfds, atoi(devNumStr1), atoi(duAStr1), atoi(duBStr1),
+                                     atoi(devNumStr2), atoi(duAStr2), atoi(duBStr2));
+        }
+        else if ((strcmp("set-duty", argv[1]) == 0) && (argc == 11))
+        {
+            const char* devNumStr1 = argv[2];
+            const char* duAStr1 = argv[3];
+            const char* duBStr1 = argv[4];
+
+            const char* devNumStr2 = argv[5];
+            const char* duAStr2 = argv[6];
+            const char* duBStr2 = argv[7];
+
+            const char* devNumStr3 = argv[8];
+            const char* duAStr3 = argv[9];
+            const char* duBStr3 = argv[10];
+            executeSetDuty3(pollfds, atoi(devNumStr1), atoi(duAStr1), atoi(duBStr1),
+                                     atoi(devNumStr2), atoi(duAStr2), atoi(duBStr2),
+                                     atoi(devNumStr3), atoi(duAStr3), atoi(duBStr3));
         }
         else
         {
